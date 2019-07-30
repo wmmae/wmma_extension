8d4340b0ff11cd953cea78115d8a78e18a6534a1
mutsuki <mutsuki@momo86.net>
2019-07-30 14:45:44 +0900

    Fix the function name to indentify target arch

--- wmma_load_vector.hpp
+++ wmma_load_vector.hpp
@@ -11,30 +11,24 @@ template <> inline __device__ half cast(const float v){return __float2half(v);}
 template <> inline __device__ half cast(const half v){return v;}
 } // namespace utils
 template <class T>
-__device__ void load_vector_sync(nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, 16, 16, 16, half, nvcuda::wmma::col_major>& frag, const T* const ptr) {
+__device__ void load_vector_sync_sm70(nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, 16, 16, 16, half, nvcuda::wmma::col_major>& frag, const T* const ptr) {
 	nvcuda::wmma::fill_fragment(frag, __float2half(0));
 	const auto warp_id = threadIdx.x & 0x1f;
-	unsigned long index_offset = 0;
-#if (CUDA_ARCH == 700)
-	index_offset = ((warp_id >> 4) << 2) + (((warp_id >> 2) & 0x1) << 3) + ((warp_id & 0x3) << 4);
+	unsigned long index_offset = ((warp_id >> 4) << 2) + (((warp_id >> 2) & 0x1) << 3) + ((warp_id & 0x3) << 4);
 	bool load_flag = (warp_id & 0x2) == 0;
 	if(load_flag) {
 		for(unsigned i = 0; i < 4; i++) {
 			frag.x[i] = utils::cast<T>(ptr[i + index_offset]);
 		}
 	}
-#elif (CUDA_ARCH == 750)
-#endif
 	__syncthreads();
 }
 
 template <class T>
-__device__ void load_vector_sync(nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, 16, 16, 16, half, nvcuda::wmma::row_major>& frag, const T* const ptr) {
+__device__ void load_vector_sync_sm70(nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, 16, 16, 16, half, nvcuda::wmma::row_major>& frag, const T* const ptr) {
 	nvcuda::wmma::fill_fragment(frag, __float2half(0));
 	const auto warp_id = threadIdx.x & 0x1f;
-	unsigned long index_offset = 0;
-#if (CUDA_ARCH == 700)
-	index_offset = (warp_id & 0x3) << 4;
+	unsigned long index_offset = (warp_id & 0x3) << 4;
 
 	bool load_flag = (warp_id == 0) || (warp_id == 1) || (warp_id == 4) || (warp_id == 5);
 	if(load_flag) {
@@ -42,18 +36,14 @@ __device__ void load_vector_sync(nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, 
 			frag.x[i] = utils::cast<T>(ptr[i + index_offset]);
 		}
 	}
-#elif (CUDA_ARCH == 750)
-#endif
 	__syncthreads();
 }
 
 template <class T>
-__device__ void load_vector_sync(nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, 16, 16, 16, half, nvcuda::wmma::col_major>& frag, const T* const ptr) {
+__device__ void load_vector_sync_sm70(nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, 16, 16, 16, half, nvcuda::wmma::col_major>& frag, const T* const ptr) {
 	nvcuda::wmma::fill_fragment(frag, __float2half(0));
 	const auto warp_id = threadIdx.x & 0x1f;
-	unsigned long index_offset = 0;
-#if (CUDA_ARCH == 700)
-	index_offset = (warp_id & 0x3) << 4;
+	unsigned long index_offset = (warp_id & 0x3) << 4;
 
 	bool load_flag = (warp_id == 0) || (warp_id == 1) || (warp_id == 4) || (warp_id == 5);
 	if(load_flag) {
@@ -61,26 +51,20 @@ __device__ void load_vector_sync(nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, 
 			frag.x[i] = utils::cast<T>(ptr[i + index_offset]);
 		}
 	}
-#elif (CUDA_ARCH == 750)
-#endif
 	__syncthreads();
 }
 
 template <class T>
-__device__ void load_vector_sync(nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, 16, 16, 16, half, nvcuda::wmma::row_major>& frag, const T* const ptr) {
+__device__ void load_vector_sync_sm_70(nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, 16, 16, 16, half, nvcuda::wmma::row_major>& frag, const T* const ptr) {
 	nvcuda::wmma::fill_fragment(frag, __float2half(0));
 	const auto warp_id = threadIdx.x & 0x1f;
-	unsigned long index_offset = 0;
-#if (CUDA_ARCH == 700)
-	index_offset = ((warp_id >> 4) << 2) + (((warp_id >> 2) & 0x1) << 3) + ((warp_id & 0x3) << 4);
+	unsigned long index_offset = ((warp_id >> 4) << 2) + (((warp_id >> 2) & 0x1) << 3) + ((warp_id & 0x3) << 4);
 	bool load_flag = (warp_id & 0x2) == 0;
 	if(load_flag) {
 		for(unsigned i = 0; i < 4; i++) {
 			frag.x[i] = utils::cast<T>(ptr[i + index_offset]);
 		}
 	}
-#elif (CUDA_ARCH == 750)
-#endif
 	__syncthreads();
 }
 } // namespace wmma
