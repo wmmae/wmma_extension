95fd07ed3d7a8c03c1d22ab5b0a163f813add135
mutsuki <mutsuki@momo86.net>
2019-11-06 14:59:22 +0900

    Fix `load_matrix_with_operation_sync_sm70` for row_major-matrix_b-sm70

--- wmma_extension.hpp
+++ wmma_extension.hpp
@@ -344,8 +344,7 @@ __device__ inline void load_matrix_with_operation_sync_sm70(nvcuda::wmma::fragme
 	const auto warp_id = threadIdx.x & 0x1f;
 	const auto start_index = (((warp_id >> 2) & 0b1) << 3) + (((warp_id >> 4) & 0b1) << 2) + ((warp_id & 0b11) << 4);
 	for (std::size_t x = 0; x < frag.num_elements; x++) {
-		const auto i = x & 0b111;
-		const auto offset = ((i >> 2) << 6) + (i & 0b11);
+		const auto offset = ((x >> 2) << 6) + (x & 0b11);
 		const auto index = start_index + offset;
 		frag.x[x] = func(x, ptr[(index >> 4) * ldm + (index & 0xf)]);
 	}
