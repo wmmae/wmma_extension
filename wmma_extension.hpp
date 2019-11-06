019dae454cb961354d43e91252883f855166b1fb
mutsuki <mutsuki@momo86.net>
2019-11-06 14:03:24 +0900

    Add `load_matrix_with_operation_sync_sm75` for row_major-matrix_a

--- wmma_extension.hpp
+++ wmma_extension.hpp
@@ -162,6 +162,20 @@ __device__ inline void load_matrix_with_operation_sync_sm75(nvcuda::wmma::fragme
 	__syncthreads();
 }
 
+template <class T, class Func>
+__device__ inline void load_matrix_with_operation_sync_sm75(nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, 16, 16, 16, half, nvcuda::wmma::row_major>& frag, const T* const ptr, const unsigned ldm, Func func) {
+	nvcuda::wmma::fill_fragment(frag, __float2half(0));
+	const auto warp_id = threadIdx.x & 0x1f;
+	const auto start_index = ((warp_id >> 2) << 4) + ((warp_id & 0b11) << 1);
+	for (std::size_t x = 0; x < frag.num_elements; x++) {
+		const auto i = x & 0b111;
+		const auto offset = (i & 0b1) + ((i & 0b10) << 6) + ((i & 0b100) << 1);
+		const auto index = start_index + offset;
+		frag.x[x] = func(x, ptr[(index >> 4) * ldm + (index & 0xf)]);
+	}
+	__syncthreads();
+}
+
 template <class T>
 __device__ inline void make_identity_matrix_sm75(nvcuda::wmma::fragment<nvcuda::wmma::accumulator, 16, 16, 16, T>& frag) {
 	nvcuda::wmma::fill_fragment(frag, utils::cast<T>(0.0f));
