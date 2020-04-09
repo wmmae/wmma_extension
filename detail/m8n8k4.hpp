315e168df572a853048cf14a8451c514090466b5
mutsuki <mutsuki@momo86.net>
2020-04-09 19:49:10 +0900

    Add `mma_sync` for `col.col.f32.f16.f16.f32`

--- detail/m8n8k4.hpp
+++ detail/m8n8k4.hpp
@@ -30,6 +30,23 @@ __device__ inline void fill_fragment(__frag_base<T, size>& f, const T v) {
 		f.x[i] = v; 
 }
 
+__device__ inline void mma_sync(fragment<nvcuda::wmma::accumulator, 8, 8, 4, float>& d, const fragment<nvcuda::wmma::matrix_a, 8, 8, 4, half, nvcuda::wmma::col_major> a, fragment<nvcuda::wmma::matrix_b, 8, 8, 4, half, nvcuda::wmma::col_major> b, const fragment<nvcuda::wmma::accumulator, 8, 8, 4, float>& c) {
+	asm(R"({
+	mma.sync.aligned.m8n8k4.col.col.f32.f16.f16.f32
+	{%0, %1, %2, %3, %4, %5, %6, %7},
+	{%8, %9},
+	{%10, %11},
+	{%12, %13, %14, %15, %16, %17, %18, %19};
+})"
+			: "=f"(d.x[0]), "=f"(d.x[1]), "=f"(d.x[2]), "=f"(d.x[3]), "=f"(d.x[4]), "=f"(d.x[5]), "=f"(d.x[6]), "=f"(d.x[7])
+			: "r"(*reinterpret_cast<const unsigned*>(a.x)), "r"(*reinterpret_cast<const unsigned*>(a.x + 2)),
+			  "r"(*reinterpret_cast<const unsigned*>(b.x)), "r"(*reinterpret_cast<const unsigned*>(b.x + 2)),
+			  "f"(c.x[0]), "f"(c.x[1]), "f"(c.x[2]), "f"(c.x[3]), "f"(c.x[4]), "f"(c.x[5]), "f"(c.x[6]), "f"(c.x[7])
+			);
+}
+
+
+
 } // namespace wmma
 } // namespace mtk
 
