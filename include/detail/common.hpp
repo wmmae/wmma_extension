111aa5ed854e0e97597d3dad7a04b28c54b59aa3
mutsuki <mutsuki@momo86.net>
2020-08-23 16:04:37 +0900

    Move `fill_zero` to common.hpp

--- include/detail/common.hpp
+++ include/detail/common.hpp
@@ -13,6 +13,23 @@ template <> inline __device__ half cast(const float v){return __float2half(v);}
 template <> inline __device__ half cast(const half v){return v;}
 } // namespace common
 } // namespace detail
+
+template <class Use, int M, int N, int K, class Layout>
+__device__ inline void fill_zero(nvcuda::wmma::fragment<Use, M, N, K, float, Layout>& frag) {
+	int4* const i4 = reinterpret_cast<int4*>(frag.x);
+	const unsigned size = sizeof(float) * nvcuda::wmma::fragment<Use, M, N, K, float, Layout>::num_elements;
+	for (unsigned i = 0; i < size / sizeof(int4); i++) {
+		i4[i] = make_int4(0, 0, 0, 0);
+	}
+}
+template <class Use, int M, int N, int K, class Layout>
+__device__ inline void fill_zero(nvcuda::wmma::fragment<Use, M, N, K, half, Layout>& frag) {
+	int4* const i4 = reinterpret_cast<int4*>(frag.x);
+	const unsigned size = sizeof(half) * nvcuda::wmma::fragment<Use, M, N, K, half, Layout>::num_elements;
+	for (unsigned i = 0; i < size / sizeof(int4); i++) {
+		i4[i] = make_int4(0, 0, 0, 0);
+	}
+}
 } // namespace wmma
 } // namespace mtk
 #endif
--- include/wmma_extension.hpp
+++ include/wmma_extension.hpp
@@ -6,26 +6,6 @@
 #include <stdio.h>
 
 namespace mtk {
-namespace wmma {
-// Common for sm_70 and sm_75
-template <class Use, int M, int N, int K, class Layout>
-__device__ inline void fill_zero(nvcuda::wmma::fragment<Use, M, N, K, float, Layout>& frag) {
-	int4* const i4 = reinterpret_cast<int4*>(frag.x);
-	const unsigned size = sizeof(float) * nvcuda::wmma::fragment<Use, M, N, K, float, Layout>::num_elements;
-	for (unsigned i = 0; i < size / sizeof(int4); i++) {
-		i4[i] = make_int4(0, 0, 0, 0);
-	}
-}
-template <class Use, int M, int N, int K, class Layout>
-__device__ inline void fill_zero(nvcuda::wmma::fragment<Use, M, N, K, half, Layout>& frag) {
-	int4* const i4 = reinterpret_cast<int4*>(frag.x);
-	const unsigned size = sizeof(half) * nvcuda::wmma::fragment<Use, M, N, K, half, Layout>::num_elements;
-	for (unsigned i = 0; i < size / sizeof(int4); i++) {
-		i4[i] = make_int4(0, 0, 0, 0);
-	}
-}
-} // namespace wmma
-
 // arch switch
 #if __CUDA_ARCH__ < 710
 namespace detail_namespace = mtk::wmma::detail::sm_70;
