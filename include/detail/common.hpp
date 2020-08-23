460310c8e4d26ac9b4346037aa570a558ed32a2d
mutsuki <mutsuki@momo86.net>
2020-08-23 15:38:54 +0900

    Create common.hpp

---
+++ include/detail/common.hpp
@@ -0,0 +1,12 @@
+#ifndef __WMMAE_DETAIL_COMMON__
+#define __WMMAE_DETAIL_COMMON__
+#include <cuda_fp16.h>
+namespace mtk {
+namespace wmma {
+namespace detail {
+namespace common {
+} // namespace common
+} // namespace detail
+} // namespace wmma
+} // namespace mtk
+#endif
