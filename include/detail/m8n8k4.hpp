08a1a1042a0103b9ceeb9db965c9e6146847de19
mutsuki <mutsuki@momo86.net>
2020-10-01 22:32:22 +0900

    Fix some syntax errors

--- include/detail/m8n8k4.hpp
+++ include/detail/m8n8k4.hpp
@@ -108,28 +108,28 @@ __device__ inline void load_matrix_sync(mtk::wmma::fragment<nvcuda::wmma::accumu
 }
 
 template <class T>
-__device__ inline void store_matrix_sync(T* const p, const fragment<nvcuda::wmma::accumulator, 8, 8, 4, half, void>& f, const unsigned ldm, nvcuda::wmma::layout_t) {
+__device__ inline void store_matrix_sync(T* const p, const fragment<nvcuda::wmma::accumulator, 8, 8, 4, half, void>& f, const unsigned ldm, const nvcuda::wmma::layout_t layout) {
 	const unsigned lane_id = mtk::wmma::detail::common::get_lane_id();
 	const unsigned col_start = ((lane_id >> 2) & 0x3) << 1;
 	const unsigned row = (lane_id & 0x3) + (lane_id >> 4);
 	if (layout == nvcuda::wmma::mem_col_major) {
 		const unsigned index = col_start * ldm + row;
 
-		p[index + 0 * ldm] = mtk::wmma::detail::common::cast<half>(f.x[col_start + 0]);
-		p[index + 1 * ldm] = mtk::wmma::detail::common::cast<half>(f.x[col_start + 1]);
+		p[index + 0 * ldm] = mtk::wmma::detail::common::cast<T>(f.x[col_start + 0]);
+		p[index + 1 * ldm] = mtk::wmma::detail::common::cast<T>(f.x[col_start + 1]);
 	} else {
 		const unsigned index = col_start + row * ldm;
 
-		p[index + 0] = mtk::wmma::detail::common::cast<half>(f.x[col_start + 0]);
-		p[index + 1] = mtk::wmma::detail::common::cast<half>(f.x[col_start + 1]);
+		p[index + 0] = mtk::wmma::detail::common::cast<T>(f.x[col_start + 0]);
+		p[index + 1] = mtk::wmma::detail::common::cast<T>(f.x[col_start + 1]);
 	}
 }
 
 template <class T>
 __device__ inline void load_matrix_sync(mtk::wmma::fragment<nvcuda::wmma::accumulator, 8, 8, 4, float, void>& f, const T* const p, const unsigned ldm, const nvcuda::wmma::layout_t layout) {
 	const unsigned lane_id = mtk::wmma::detail::common::get_lane_id();
-	const row_offset = lane_id & 0x1;
-	const col_offset = (lane_id & 0x2) >> 1;
+	const unsigned row_offset = lane_id & 0x1;
+	const unsigned col_offset = (lane_id & 0x2) >> 1;
 
 	if (layout == nvcuda::wmma::mem_col_major) {
 #pragma unroll
@@ -151,26 +151,25 @@ __device__ inline void load_matrix_sync(mtk::wmma::fragment<nvcuda::wmma::accumu
 }
 
 template <class T>
-__device__ inline void store_matrix_sync(T* const p, const fragment<nvcuda::wmma::accumulator, 8, 8, 4, float, void>& f, const unsigned ldm, nvcuda::wmma::layout_t) {
+__device__ inline void store_matrix_sync(T* const p, const fragment<nvcuda::wmma::accumulator, 8, 8, 4, float, void>& f, const unsigned ldm, const nvcuda::wmma::layout_t layout) {
 	const unsigned lane_id = mtk::wmma::detail::common::get_lane_id();
-	const unsigned matrix_block_id = lane_id & 0x3;
 	const unsigned row = (lane_id & 0x1) + ((lane_id & 0x18) >> 2);
 	const unsigned col_offset = lane_id & 0x6;
 	const unsigned frag_index = ((lane_id >> 2) & 0x2) + (lane_id & 0x4);
 
 	if (layout == nvcuda::wmma::mem_col_major) {
-		p[row + (col_offset + 0) * ldm] = f.x[frag_index + 0];
-		p[row + (col_offset + 1) * ldm] = f.x[frag_index + 1];
+		p[row + (col_offset + 0) * ldm] = mtk::wmma::detail::common::cast<T>(f.x[frag_index + 0]);;
+		p[row + (col_offset + 1) * ldm] = mtk::wmma::detail::common::cast<T>(f.x[frag_index + 1]);
 	}else {
 		const unsigned offset = row * ldm + col_offset;
-		p[offset + 0] = f.x[frag_index + 0];
-		p[offset + 1] = f.x[frag_index + 1];
+		p[offset + 0] = mtk::wmma::detail::common::cast<T>(f.x[frag_index + 0]);
+		p[offset + 1] = mtk::wmma::detail::common::cast<T>(f.x[frag_index + 1]);
 	}
 }
 
 #define MMA_F32_F32(A_LAYOUT, B_LAYOUT) \
 __device__ inline void mma_sync(fragment<nvcuda::wmma::accumulator, 8, 8, 4, float>& d, const fragment<nvcuda::wmma::matrix_a, 8, 8, 4, half, nvcuda::wmma::A_LAYOUT##_major>& a, fragment<nvcuda::wmma::matrix_b, 8, 8, 4, half, nvcuda::wmma::B_LAYOUT##_major>& b, const fragment<nvcuda::wmma::accumulator, 8, 8, 4, float>& c) { \
-	asm("{mma.sync.aligned.m8n8k4."#A_LAYOUT"."#B_LAYOUT".f32.f16.f16.f32 {%%0, %%1, %%2, %%3, %%4, %%5, %%6, %%7}, {%%8, %%9}, {%%10, %%11}, {%%12, %%13, %%14, %%15, %%16, %%17, %%18, %%19};}" : "=f"(d.x[0]), "=f"(d.x[1]), "=f"(d.x[2]), "=f"(d.x[3]), "=f"(d.x[4]), "=f"(d.x[5]), "=f"(d.x[6]), "=f"(d.x[7]) : "r"(*reinterpret_cast<const unsigned*>(a.x)), "r"(*reinterpret_cast<const unsigned*>(a.x + 2)), "r"(*reinterpret_cast<const unsigned*>(b.x)), "r"(*reinterpret_cast<const unsigned*>(b.x + 2)), "f"(c.x[0]), "f"(c.x[1]), "f"(c.x[2]), "f"(c.x[3]), "f"(c.x[4]), "f"(c.x[5]), "f"(c.x[6]), "f"(c.x[7])); \
+	asm("{mma.sync.aligned.m8n8k4."#A_LAYOUT"."#B_LAYOUT".f32.f16.f16.f32 {%0, %1, %2, %3, %4, %5, %6, %7}, {%8, %9}, {%10, %11}, {%12, %13, %14, %15, %16, %17, %18, %19};}" : "=f"(d.x[0]), "=f"(d.x[1]), "=f"(d.x[2]), "=f"(d.x[3]), "=f"(d.x[4]), "=f"(d.x[5]), "=f"(d.x[6]), "=f"(d.x[7]) : "r"(*reinterpret_cast<const unsigned*>(a.x)), "r"(*reinterpret_cast<const unsigned*>(a.x + 2)), "r"(*reinterpret_cast<const unsigned*>(b.x)), "r"(*reinterpret_cast<const unsigned*>(b.x + 2)), "f"(c.x[0]), "f"(c.x[1]), "f"(c.x[2]), "f"(c.x[3]), "f"(c.x[4]), "f"(c.x[5]), "f"(c.x[6]), "f"(c.x[7])); \
 }
 
 MMA_F32_F32(col, col);
@@ -180,7 +179,7 @@ MMA_F32_F32(row, row);
 
 #define MMA_F16_F32(A_LAYOUT, B_LAYOUT) \
 __device__ inline void mma_sync(fragment<nvcuda::wmma::accumulator, 8, 8, 4, half>& d, const fragment<nvcuda::wmma::matrix_a, 8, 8, 4, half, nvcuda::wmma::A_LAYOUT##_major>& a, fragment<nvcuda::wmma::matrix_b, 8, 8, 4, half, nvcuda::wmma::B_LAYOUT##_major>& b, const fragment<nvcuda::wmma::accumulator, 8, 8, 4, float>& c) { \
-	asm("{mma.sync.aligned.m8n8k4."#A_LAYOUT"."#B_LAYOUT".f16.f16.f16.f32 {%%0, %%1, %%2, %%3}, {%%4, %%5}, {%%6, %%7}, {%%8, %%9, %%10, %%11, %%12, %%13, %%14, %%15};}" : "=r"(*reinterpret_cast<const unsigned*>(d.x + 0)), "=r"(*reinterpret_cast<const unsigned*>(d.x + 2)), "=r"(*reinterpret_cast<const unsigned*>(d.x + 4)), "=r"(*reinterpret_cast<const unsigned*>(d.x + 6)) : "r"(*reinterpret_cast<const unsigned*>(a.x)), "r"(*reinterpret_cast<const unsigned*>(a.x + 2)), "r"(*reinterpret_cast<const unsigned*>(b.x)), "r"(*reinterpret_cast<const unsigned*>(b.x + 2)), "f"(c.x[0]), "f"(c.x[1]), "f"(c.x[2]), "f"(c.x[3]), "f"(c.x[4]), "f"(c.x[5]), "f"(c.x[6]), "f"(c.x[7])); \
+	asm("{mma.sync.aligned.m8n8k4."#A_LAYOUT"."#B_LAYOUT".f16.f16.f16.f32 {%0, %1, %2, %3}, {%4, %5}, {%6, %7}, {%8, %9, %10, %11, %12, %13, %14, %15};}" : "=r"(*reinterpret_cast<unsigned*>(d.x + 0)), "=r"(*reinterpret_cast<unsigned*>(d.x + 2)), "=r"(*reinterpret_cast<unsigned*>(d.x + 4)), "=r"(*reinterpret_cast<unsigned*>(d.x + 6)) : "r"(*reinterpret_cast<const unsigned*>(a.x)), "r"(*reinterpret_cast<const unsigned*>(a.x + 2)), "r"(*reinterpret_cast<const unsigned*>(b.x)), "r"(*reinterpret_cast<const unsigned*>(b.x + 2)), "f"(c.x[0]), "f"(c.x[1]), "f"(c.x[2]), "f"(c.x[3]), "f"(c.x[4]), "f"(c.x[5]), "f"(c.x[6]), "f"(c.x[7])); \
 }
 
 MMA_F16_F32(col, col);
@@ -190,7 +189,7 @@ MMA_F16_F32(row, row);
 
 #define MMA_F32_F16(A_LAYOUT, B_LAYOUT) \
 __device__ inline void mma_sync(fragment<nvcuda::wmma::accumulator, 8, 8, 4, float>& d, const fragment<nvcuda::wmma::matrix_a, 8, 8, 4, half, nvcuda::wmma::A_LAYOUT##_major>& a, fragment<nvcuda::wmma::matrix_b, 8, 8, 4, half, nvcuda::wmma::B_LAYOUT##_major>& b, const fragment<nvcuda::wmma::accumulator, 8, 8, 4, half>& c) { \
-	asm("{mma.sync.aligned.m8n8k4."#A_LAYOUT"."#B_LAYOUT".f32.f16.f16.f16 {%%0, %%1, %%2, %%3, %%4, %%5, %%6, %%7}, {%%8, %%9}, {%%10, %%11}, {%%12, %%13, %%14, %%15};}" : "=f"(d.x[0]), "=f"(d.x[1]), "=f"(d.x[2]), "=f"(d.x[3]), "=f"(d.x[4]), "=f"(d.x[5]), "=f"(d.x[6]), "=f"(d.x[7]) : "r"(*reinterpret_cast<const unsigned*>(a.x)), "r"(*reinterpret_cast<const unsigned*>(a.x + 2)), "r"(*reinterpret_cast<const unsigned*>(b.x)), "r"(*reinterpret_cast<const unsigned*>(b.x + 2)), "r"(*reinterpret_cast<const unsigned*>(c.x + 0)), "r"(*reinterpret_cast<const unsigned*>(c.x + 2)), "r"(*reinterpret_cast<const unsigned*>(c.x + 4)), "r"(*reinterpret_cast<const unsigned*>(c.x + 6))); \
+	asm("{mma.sync.aligned.m8n8k4."#A_LAYOUT"."#B_LAYOUT".f32.f16.f16.f16 {%0, %1, %2, %3, %4, %5, %6, %7}, {%8, %9}, {%10, %11}, {%12, %13, %14, %15};}" : "=f"(d.x[0]), "=f"(d.x[1]), "=f"(d.x[2]), "=f"(d.x[3]), "=f"(d.x[4]), "=f"(d.x[5]), "=f"(d.x[6]), "=f"(d.x[7]) : "r"(*reinterpret_cast<const unsigned*>(a.x)), "r"(*reinterpret_cast<const unsigned*>(a.x + 2)), "r"(*reinterpret_cast<const unsigned*>(b.x)), "r"(*reinterpret_cast<const unsigned*>(b.x + 2)), "r"(*reinterpret_cast<const unsigned*>(c.x + 0)), "r"(*reinterpret_cast<const unsigned*>(c.x + 2)), "r"(*reinterpret_cast<const unsigned*>(c.x + 4)), "r"(*reinterpret_cast<const unsigned*>(c.x + 6))); \
 }
 
 MMA_F32_F16(col, col);
@@ -200,7 +199,7 @@ MMA_F32_F16(row, row);
 
 #define MMA_F16_F16(A_LAYOUT, B_LAYOUT) \
 __device__ inline void mma_sync(fragment<nvcuda::wmma::accumulator, 8, 8, 4, half>& d, const fragment<nvcuda::wmma::matrix_a, 8, 8, 4, half, nvcuda::wmma::A_LAYOUT##_major>& a, fragment<nvcuda::wmma::matrix_b, 8, 8, 4, half, nvcuda::wmma::B_LAYOUT##_major>& b, const fragment<nvcuda::wmma::accumulator, 8, 8, 4, half>& c) { \
-	asm("{mma.sync.aligned.m8n8k4."#A_LAYOUT"."#B_LAYOUT".f16.f16.f16.f16 {%%0, %%1, %%2, %%3}, {%%4, %%5}, {%%6, %%7}, {%%8, %%9, %%10, %%11};}" : "=r"(*reinterpret_cast<const unsigned*>(d.x + 0)), "=r"(*reinterpret_cast<const unsigned*>(d.x + 2)), "=r"(*reinterpret_cast<const unsigned*>(d.x + 4)), "=r"(*reinterpret_cast<const unsigned*>(d.x + 6)) : "r"(*reinterpret_cast<const unsigned*>(a.x)), "r"(*reinterpret_cast<const unsigned*>(a.x + 2)), "r"(*reinterpret_cast<const unsigned*>(b.x)), "r"(*reinterpret_cast<const unsigned*>(b.x + 2)), "r"(*reinterpret_cast<const unsigned*>(c.x + 0)), "r"(*reinterpret_cast<const unsigned*>(c.x + 2)), "r"(*reinterpret_cast<const unsigned*>(c.x + 4)), "r"(*reinterpret_cast<const unsigned*>(c.x + 6))); \
+	asm("{mma.sync.aligned.m8n8k4."#A_LAYOUT"."#B_LAYOUT".f16.f16.f16.f16 {%0, %1, %2, %3}, {%4, %5}, {%6, %7}, {%8, %9, %10, %11};}" : "=r"(*reinterpret_cast<unsigned*>(d.x + 0)), "=r"(*reinterpret_cast<unsigned*>(d.x + 2)), "=r"(*reinterpret_cast<unsigned*>(d.x + 4)), "=r"(*reinterpret_cast<unsigned*>(d.x + 6)) : "r"(*reinterpret_cast<const unsigned*>(a.x)), "r"(*reinterpret_cast<const unsigned*>(a.x + 2)), "r"(*reinterpret_cast<const unsigned*>(b.x)), "r"(*reinterpret_cast<const unsigned*>(b.x + 2)), "r"(*reinterpret_cast<const unsigned*>(c.x + 0)), "r"(*reinterpret_cast<const unsigned*>(c.x + 2)), "r"(*reinterpret_cast<const unsigned*>(c.x + 4)), "r"(*reinterpret_cast<const unsigned*>(c.x + 6))); \
 }
 
 MMA_F16_F16(col, col);
