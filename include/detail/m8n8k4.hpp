17af98e1917b5894e47a328b918d50cf0f52a849
mutsuki <mutsuki@momo86.net>
2020-10-01 22:51:04 +0900

    Add the definition of `print_fragment` for `mtk::wmma::fragment`

--- include/detail/m8n8k4.hpp
+++ include/detail/m8n8k4.hpp
@@ -207,6 +207,32 @@ MMA_F16_F16(row, col);
 MMA_F16_F16(col, row);
 MMA_F16_F16(row, row);
 
+// Debug function
+template <class MatrixType, int M, int N, int K, class MemMajor, class T>
+__device__ inline void print_fragment(const mtk::wmma::fragment<MatrixType, M, N, K, T, MemMajor>& frag, const char* name = "") {
+	if ((threadIdx.x & 0x1f) == 0) {
+		if (name[0] != '\0') {
+			printf("%s = \n", name);
+		}
+	}
+	for (unsigned i = 0; i < warpSize; i++) {
+		if (i == (threadIdx.x & 0x1f)) {
+			for (unsigned j = 0; j < frag.num_elements; j++) {
+				const auto v = mtk::wmma::detail::common::cast<float>(frag.x[j]);
+				if (v == 0.0f) {
+					printf(" %.3e ", 0.0f);
+				} else if (v > 0) {
+					printf(" %.3e ", v);
+				} else {
+					printf("%.3e ", v);
+				}
+			}
+			printf("\n");
+		}
+		__syncthreads();
+	}
+}
+
 } // namespace wmma
 } // namespace mtk
 
