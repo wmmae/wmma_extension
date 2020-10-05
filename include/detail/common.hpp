9c1a6ab5288affcc4b780efdee6a61431f564f18
mutsuki <mutsuki@momo86.net>
2020-10-05 22:40:04 +0900

    Add the definition of `fill_zero` for tf32

--- include/detail/common.hpp
+++ include/detail/common.hpp
@@ -35,6 +35,17 @@ __device__ inline void fill_zero(nvcuda::wmma::fragment<Use, M, N, K, half, Layo
 		i4[i] = make_int4(0, 0, 0, 0);
 	}
 }
+
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 800
+template <class Use, int M, int N, int K, class Layout>
+__device__ inline void fill_zero(nvcuda::wmma::fragment<Use, M, N, K, nvcuda::wmma::precision::tf32, Layout>& frag) {
+	int4* const i4 = reinterpret_cast<int4*>(frag.x);
+	const unsigned size = sizeof(float) * nvcuda::wmma::fragment<Use, M, N, K, nvcuda::wmma::precision::tf32, Layout>::num_elements;
+	for (unsigned i = 0; i < size / sizeof(int4); i++) {
+		i4[i] = make_int4(0, 0, 0, 0);
+	}
+}
+#endif
 } // namespace wmma
 } // namespace mtk
 #endif
