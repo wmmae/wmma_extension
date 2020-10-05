41c21238030d9d38428507c86c02485899a55432
mutsuki <mutsuki@momo86.net>
2020-10-05 22:30:03 +0900

    Update some bugs

--- include/detail/sm_80_tf32.hpp
+++ include/detail/sm_80_tf32.hpp
@@ -216,6 +216,7 @@ __device__ inline void load_matrix_with_operation_sync(nvcuda::wmma::fragment<nv
 	const unsigned start_index = ((lane_id & 0x3) << 4) + ((lane_id & 0x4) >> 2);
 	for (std::size_t x = 0; x < frag.num_elements; x++) {
 		const unsigned offset = ((x & 0x2) << 5) + ((x & 0x1) << 3);
+		const unsigned index = start_index + offset;
 		frag.x[x] = func(x, ptr[(index >> 4) * ldm + (index & 0xf)]);
 	}
 	__syncthreads();
@@ -340,10 +341,12 @@ __device__ inline void make_direct_product_fragment(
 	auto a8 = a_ptr[(lane_id >> 2) + 8];
 
 	// __float2tf32
+#if __CUDA_ARCH__ >= 800
 	if (lane_id & 0x1) {
-		a0 = a0 - __float2tf32(a0);
-		a8 = a8 - __float2tf32(a8);
+		a0 = a0 - __float_to_tf32(a0);
+		a8 = a8 - __float_to_tf32(a8);
 	}
+#endif
 
 	frag_a.x[0] = a0;
 	frag_a.x[1] = a8;
@@ -364,13 +367,15 @@ __device__ inline void make_direct_product_fragment(
 	auto b8 = b_ptr[(lane_id >> 2) + 8];
 
 	// __float2tf32
+#if __CUDA_ARCH__ >= 800
 	if (lane_id & 0x1) {
-		b0 = b0 - __float2tf32(b0);
-		b8 = b8 - __float2tf32(b8);
+		b0 = b0 - __float_to_tf32(b0);
+		b8 = b8 - __float_to_tf32(b8);
 	}
+#endif
 
-	frag_a.x[0] = b0;
-	frag_a.x[2] = b8;
+	frag_b.x[0] = b0;
+	frag_b.x[2] = b8;
 }
 } // namespace sm_80
 } // namespace detail
