1f2bd97a0b775a37f5fa53f1d427ff73d7355fbd
mutsuki <mutsuki@momo86.net>
2020-10-06 12:11:01 +0900

    Add the definition of `to_tf32` and use it to converting

--- include/detail/common.hpp
+++ include/detail/common.hpp
@@ -5,12 +5,28 @@ namespace mtk {
 namespace wmma {
 namespace detail {
 namespace common {
-template <class T> inline __device__ __host__ T cast(const float v);
-template <class T> inline __device__ __host__ T cast(const half v);
-template <> inline __device__ __host__ float cast(const float v){return v;}
-template <> inline __device__ __host__ float cast(const half v){return __half2float(v);}
-template <> inline __device__ __host__ half cast(const float v){return __float2half(v);}
-template <> inline __device__ __host__ half cast(const half v){return v;}
+template <class T>
+struct storage_t {using type = T;};
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 800
+template <> struct storage_t<nvcuda::wmma::precision::tf32> {using type = float;};
+__device__ inline float to_tf32(const float a) {
+	float ret;
+    asm("{.reg .b32 %mr;\n"
+        "cvt.rna.tf32.f32 %mr, %1;\n"
+        "mov.b32 %0, %mr;}\n" : "=f"(ret) : "f"(a));
+    return ret;
+}
+#endif
+template <class T> inline __device__ __host__ typename storage_t<T>::type cast(const float v);
+template <class T> inline __device__ __host__ typename storage_t<T>::type cast(const half v);
+template <> inline __device__ __host__ typename storage_t<float>::type cast<float>(const float v){return v;}
+template <> inline __device__ __host__ typename storage_t<half >::type cast<half >(const float v){return __float2half(v);}
+template <> inline __device__ __host__ typename storage_t<float>::type cast<float>(const half v){return __half2float(v);}
+template <> inline __device__ __host__ typename storage_t<half >::type cast<half >(const half v){return v;}
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 800
+template <> inline __device__ __host__ typename storage_t<nvcuda::wmma::precision::tf32>::type cast<nvcuda::wmma::precision::tf32>(const float v){return to_tf32(v);}
+template <> inline __device__ __host__ typename storage_t<nvcuda::wmma::precision::tf32>::type cast<nvcuda::wmma::precision::tf32>(const half  v){return to_tf32(__half2float(v));}
+#endif
 inline __device__ unsigned get_lane_id() {
 	unsigned lane_id;
 	asm(R"({mov.s32 %0, %laneid;})":"=r"(lane_id));
