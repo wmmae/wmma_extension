bb5877b5246a57435b96486c697fbcca7d1868b9
mutsuki <mutsuki@momo86.net>
2021-03-09 22:49:14 +0900

    Add `get_M`, `get_N` and `layout_switch`

--- include/wmma_extension/detail/common.hpp
+++ include/wmma_extension/detail/common.hpp
@@ -55,9 +55,23 @@ inline __device__ unsigned get_lane_id() {
 	asm(R"({mov.s32 %0, %laneid;})":"=r"(lane_id));
 	return lane_id;
 }
+
+template <class Use, int M, int N, int K> struct get_M;
+template <int M, int N, int K> struct get_M<nvcuda::wmma::matrix_a   , M, N, K>{static const int value = M;};
+template <int M, int N, int K> struct get_M<nvcuda::wmma::matrix_b   , M, N, K>{static const int value = K;};
+template <int M, int N, int K> struct get_M<nvcuda::wmma::accumulator, M, N, K>{static const int value = M;};
+
+template <class Use, int M, int N, int K> struct get_N;
+template <int M, int N, int K> struct get_N<nvcuda::wmma::matrix_a   , M, N, K>{static const int value = K;};
+template <int M, int N, int K> struct get_N<nvcuda::wmma::matrix_b   , M, N, K>{static const int value = N;};
+template <int M, int N, int K> struct get_N<nvcuda::wmma::accumulator, M, N, K>{static const int value = N;};
+
+template <class Layout, int col_value, int row_calue> struct layout_switch;
+template <int col_value, int row_calue> struct layout_switch<nvcuda::wmma::col_major, col_value, row_value> {static const int value = col_value;};
+template <int col_value, int row_calue> struct layout_switch<nvcuda::wmma::row_major, col_value, row_value> {static const int value = row_value;};
+
 } // namespace common
 } // namespace detail
-
 template <class Use, int M, int N, int K, class Layout>
 __device__ inline void fill_zero(nvcuda::wmma::fragment<Use, M, N, K, float, Layout>& frag) {
 	int4* const i4 = reinterpret_cast<int4*>(frag.x);
