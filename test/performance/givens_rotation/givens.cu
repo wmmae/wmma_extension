b97112629e65d5fb8a8358910966ba4990092f78
mutsuki <mutsuki@momo86.net>
2021-08-04 10:21:58 +0900

    Update Given's rotation benchmark

--- test/performance/givens_rotation/givens.cu
+++ test/performance/givens_rotation/givens.cu
@@ -20,10 +20,12 @@ __device__ void cp_matrix(
 template <unsigned DIM, class GivensMatGen>
 __global__ void batched_givens_kernel(
 		half* const ptr,
-		const unsigned gi, const unsigned gj,
-		const float theta,
 		const unsigned batch_size) {
 	__shared__ half smem[DIM * DIM * block_size / warp_size];
+
+	const unsigned gi = 5;
+	const unsigned gj = 6;
+	const float theta = M_PI / 6;
 	half* const smem_ptr = smem + DIM * DIM * (threadIdx.x / warp_size);
 
 	const unsigned matrix_id = threadIdx.x + blockIdx.x * blockDim.x / warp_size;
@@ -69,14 +71,16 @@ struct GivensMatGenWMMA {
 		__syncwarp();
 		const auto lane_id = (threadIdx.x & 0x1f);
 		if (lane_id < 16) {
-			smem_h2_ptr[(threadIdx.x & 0x1f)] = __float2half2_rn(0.f);
+			smem[(1 + DIM) * lane_id] = __float2half(1.f);
 		}
 		__syncwarp();
 		if (lane_id == 0) {
-			smem[gi + gi * DIM] = __float2half( cosf(theta));
-			smem[gi + gj * DIM] = __float2half(-sinf(theta));
-			smem[gj + gi * DIM] = __float2half( sinf(theta));
-			smem[gj + gj * DIM] = __float2half( cosf(theta));
+			const auto c = __float2half(__cosf(theta));
+			const auto s = __float2half(__sinf(theta));
+			smem[gi + gi * DIM] = c;
+			smem[gi + gj * DIM] =-s;
+			smem[gj + gi * DIM] = s;
+			smem[gj + gj * DIM] = c;
 		}
 		__syncwarp();
 		nvcuda::wmma::load_matrix_sync(frag, smem, DIM);
@@ -94,31 +98,43 @@ struct GivensMatGenWMMAe {
 			half* const
 			) const {
 		mtk::wmma::fill_zero(frag);
-		mtk::wmma::foreach_ij<decltype(frag)>(
-				[&](const unsigned* frag_index_list, const unsigned fragment_index_count, const unsigned i, const unsigned j) {
-					if (i == j) {
-            for (unsigned f = 0; f < fragment_index_count; f++)
-                frag.x[frag_index_list[f]] = __float2half(1.f);
-					}
-
-					if (i == gi) {
-						if (j == gi) {
-							for (unsigned f = 0; f < fragment_index_count; f++)
-								frag.x[frag_index_list[f]] = __float2half( cosf(theta));
-						} else if (j == gj) {
-							for (unsigned f = 0; f < fragment_index_count; f++)
-								frag.x[frag_index_list[f]] = __float2half(-sinf(theta));
-						}
-					} else if (i == gi) {
-						if (j == gi) {
-							for (unsigned f = 0; f < fragment_index_count; f++)
-								frag.x[frag_index_list[f]] = __float2half( sinf(theta));
-						} else if (j == gj) {
-							for (unsigned f = 0; f < fragment_index_count; f++)
-								frag.x[frag_index_list[f]] = __float2half( cosf(theta));
-						}
-					}
-        });
+		unsigned tid_list[2];
+		unsigned fid_list[2];
+		unsigned list_size;
+		for (unsigned i = 0; i < DIM; i++) {
+			mtk::wmma::map<decltype(frag)>(tid_list, fid_list, list_size, i, i);
+			for (unsigned k = 0; k < list_size; k++) {
+				if (threadIdx.x == tid_list[k]) {
+					frag.x[fid_list[k]] = 1.0f;
+				}
+			}
+		}
+		const auto c = __float2half(__cosf(theta));
+		mtk::wmma::map<decltype(frag)>(tid_list, fid_list, list_size, gi, gi);
+		for (unsigned k = 0; k < list_size; k++) {
+			if (threadIdx.x == tid_list[k]) {
+				frag.x[fid_list[k]] = c;
+			}
+		}
+		mtk::wmma::map<decltype(frag)>(tid_list, fid_list, list_size, gj, gj);
+		for (unsigned k = 0; k < list_size; k++) {
+			if (threadIdx.x == tid_list[k]) {
+				frag.x[fid_list[k]] = c;
+			}
+		}
+		const auto s = __float2half(__sinf(theta));
+		mtk::wmma::map<decltype(frag)>(tid_list, fid_list, list_size, gi, gj);
+		for (unsigned k = 0; k < list_size; k++) {
+			if (threadIdx.x == tid_list[k]) {
+				frag.x[fid_list[k]] = -s;
+			}
+		}
+		mtk::wmma::map<decltype(frag)>(tid_list, fid_list, list_size, gj, gi);
+		for (unsigned k = 0; k < list_size; k++) {
+			if (threadIdx.x == tid_list[k]) {
+				frag.x[fid_list[k]] = s;
+			}
+		}
 	}
 };
 
@@ -130,19 +146,14 @@ template <> std::string get_class_name<GivensMatGenWMMAe<16>>() {return "wmmae_1
 template <unsigned DIM, class GivensMatGen>
 void batched_givens(
 		half* const ptr,
-		const unsigned gi,
-		const unsigned gj,
-		const float theta,
 		const unsigned batch_size
 		) {
 	const unsigned grid_size = (batch_size * warp_size + block_size - 1) / block_size;
-	batched_givens_kernel<DIM, GivensMatGen><<<grid_size, block_size>>>(ptr, gi, gj, theta, batch_size);
+	batched_givens_kernel<DIM, GivensMatGen><<<grid_size, block_size>>>(ptr, batch_size);
 }
 
 template <unsigned DIM, class GivensMatGen>
 void test_batched_kernel(
-		const unsigned gi,
-		const unsigned gj,
 		const unsigned batch_size
 		) {
 	half* input_matrix;
@@ -151,22 +162,21 @@ void test_batched_kernel(
 	for (unsigned c = 0; c < test_count; c++) {
 		batched_givens<DIM, GivensMatGen>(
 				input_matrix,
-				gi, gj,
-				M_PI / 6,
 				batch_size);
 	}
 	cudaDeviceSynchronize();
 	const auto end_clock = std::chrono::system_clock::now();
 	cudaFree(input_matrix);
 
-	const auto elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end_clock - start_clock).count() / static_cast<double>(test_count);
+	const auto elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end_clock - start_clock).count() / static_cast<double>(test_count) * 1e-6;
 
-	std::printf("%u,%s,%u,%u,%e\n", batch_size, get_class_name<GivensMatGen>().c_str(), gi, gj, elapsed_time);
+	std::printf("%u,%s,%e\n", batch_size, get_class_name<GivensMatGen>().c_str(), elapsed_time);
 }
 
 int main() {
-	for (unsigned i = 5; i <= 22; i++) {
-		test_batched_kernel<16, GivensMatGenWMMA <16>>(0, 1, 1u << i);
-		test_batched_kernel<16, GivensMatGenWMMAe<16>>(0, 1, 1u << i);
+	std::printf("batch_size,api,time\n");
+	for (unsigned i = 10; i <= 24; i++) {
+		test_batched_kernel<16, GivensMatGenWMMA <16>>(1u << i);
+		test_batched_kernel<16, GivensMatGenWMMAe<16>>(1u << i);
 	}
 }
