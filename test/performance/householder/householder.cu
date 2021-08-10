0faa4f6eeacb186818f8db39e132214e98230305
mutsuki <mutsuki@momo86.net>
2021-08-10 14:53:36 +0900

    Update matrix size of householder benchmark

--- test/performance/householder/householder.cu
+++ test/performance/householder/householder.cu
@@ -5,6 +5,7 @@
 constexpr unsigned warp_size = 32;
 constexpr unsigned block_size = 256;
 constexpr unsigned test_count = 256;
+constexpr unsigned warp_dim = 16;
 
 template <unsigned DIM>
 __device__ void cp_matrix(
@@ -30,27 +31,38 @@ __global__ void batched_householder_kernel(
 	const unsigned matrix_id = threadIdx.x + blockIdx.x * blockDim.x / warp_size;
 	if (matrix_id >= batch_size) return;
 
-	cp_matrix<DIM>(
+	cp_matrix<warp_dim>(
 			reinterpret_cast<half2*>(smem_mat_ptr),
 			reinterpret_cast<half2*>(ptr + DIM * DIM * ((threadIdx.x + block_size / warp_size * blockIdx.x) / warp_size))
 			);
 
-	nvcuda::wmma::fragment<nvcuda::wmma::accumulator, DIM, DIM, DIM, half> frag_c;
-	nvcuda::wmma::fill_fragment(frag_c, 0.f);
+	nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, warp_dim, warp_dim, warp_dim, half, nvcuda::wmma::col_major> frag_b[warp_dim * warp_dim / (warp_dim * warp_dim)];
+	for (unsigned i = 0; i < DIM / warp_dim; i += 1) {
+		for (unsigned j = 0; j < DIM / warp_dim; j += 1) {
+			nvcuda::wmma::load_matrix_sync(frag_b[i + j * (DIM / warp_dim)], smem_mat_ptr + j * warp_dim + DIM * warp_dim * i, DIM);
+		}
+	}
 
-	nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, DIM, DIM, DIM, half, nvcuda::wmma::col_major> frag_b;
-	nvcuda::wmma::load_matrix_sync(frag_b, smem_mat_ptr, DIM);
 	if ((threadIdx.x & 0x1f) < DIM) {
 		smem_vec_ptr[(threadIdx.x & 0x1f)] = smem_mat_ptr[(threadIdx.x & 0x1f)];
 	}
 	__syncwarp();
 
-	nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, DIM, DIM, DIM, half, nvcuda::wmma::col_major> frag_a;
+	nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, warp_dim, warp_dim, warp_dim, half, nvcuda::wmma::col_major> frag_a[warp_dim * warp_dim / (warp_dim * warp_dim)];
 	HouseholderMatGen{}(frag_a, smem_mat_ptr, smem_vec_ptr);
 
-	nvcuda::wmma::mma_sync(frag_c, frag_a, frag_b, frag_c);
 
-	nvcuda::wmma::store_matrix_sync(smem_mat_ptr, frag_c, DIM, nvcuda::wmma::mem_col_major);
+
+	for (unsigned i = 0; i < DIM / warp_dim; i += 1) {
+		for (unsigned j = 0; j < DIM / warp_dim; j += 1) {
+			nvcuda::wmma::fragment<nvcuda::wmma::accumulator, warp_dim, warp_dim, warp_dim, half> frag_c;
+			nvcuda::wmma::fill_fragment(frag_c, 0.f);
+			for (unsigned k = 0; k < DIM / warp_dim; k += 1) {
+				nvcuda::wmma::mma_sync(frag_c, frag_a[i + k * (DIM / warp_dim)], frag_b[k + j * (DIM / warp_dim)], frag_c);
+			}
+			nvcuda::wmma::store_matrix_sync(smem_mat_ptr + i * warp_dim + j * warp_dim * DIM, frag_c, DIM, nvcuda::wmma::mem_col_major);
+		}
+	}
 	cp_matrix<DIM>(
 			reinterpret_cast<half2*>(ptr + DIM * DIM * ((threadIdx.x + block_size / warp_size * blockIdx.x) / warp_size)),
 			reinterpret_cast<half2*>(smem_mat_ptr)
@@ -60,7 +72,7 @@ __global__ void batched_householder_kernel(
 template <unsigned DIM>
 struct HouseholderMatGenWMMA {
 	__device__ void operator()(
-			nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, DIM, DIM, DIM, half, nvcuda::wmma::col_major>& frag,
+			nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, warp_dim, warp_dim, warp_dim, half, nvcuda::wmma::col_major>* frag,
 			half* const smem_mat,
 			half* const smem_vec
 			) const {
@@ -77,27 +89,35 @@ struct HouseholderMatGenWMMA {
 			__syncwarp();
 			smem_mat[index] = v;
 		}
-		nvcuda::wmma::load_matrix_sync(frag, smem_mat, DIM);
+		for (unsigned i = 0; i < DIM / warp_dim; i += 1) {
+			for (unsigned j = 0; j < DIM / warp_dim; j += 1) {
+				nvcuda::wmma::load_matrix_sync(frag[i + j * (DIM / warp_dim)], smem_mat + i * warp_dim + DIM * warp_dim * j, DIM);
+			}
+		}
 	}
 };
 
 template <unsigned DIM>
 struct HouseholderMatGenWMMAe {
 	__device__ void operator()(
-			nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, DIM, DIM, DIM, half, nvcuda::wmma::col_major>& frag,
+			nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, warp_dim, warp_dim, warp_dim, half, nvcuda::wmma::col_major>* frag,
 			half* const,
 			const half* const smem_vec
 			) const {
-		mtk::wmma::foreach_ij<decltype(frag)>(
-				[&](const unsigned *list, const unsigned list_size, const unsigned i, const unsigned j) {
-					half v = smem_vec[i] * smem_vec[j] * __float2half(-2.f);
-					if (i == j) {
-						v += __float2half(1.f);
-					}
-					__syncwarp();
+		mtk::wmma::foreach_ij<nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, warp_dim, warp_dim, warp_dim, half, nvcuda::wmma::col_major>>(
+				[&](const unsigned *list, const unsigned list_size, const unsigned m, const unsigned n) {
+					for (unsigned i = 0; i < DIM; i += warp_dim) {
+						for (unsigned j = 0; j < DIM; j += warp_dim) {
+							half v = smem_vec[i + m] * smem_vec[j + n] * __float2half(-2.f);
+							if (m == n && j == i) {
+								v += __float2half(1.f);
+							}
+							__syncwarp();
 #pragma unroll
-					for (unsigned f = 0; f < list_size; f++) {
-						frag.x[f] = v;
+							for (unsigned f = 0; f < list_size; f++) {
+								frag[i / warp_dim + j / warp_dim * (DIM / warp_dim)].x[f] = v;
+							}
+						}
 					}
 				});
 		__syncwarp();
@@ -108,6 +128,8 @@ template <class T>
 std::string get_class_name();
 template <> std::string get_class_name<HouseholderMatGenWMMA <16>>() {return "wmma_16";}
 template <> std::string get_class_name<HouseholderMatGenWMMAe<16>>() {return "wmmae_16";}
+template <> std::string get_class_name<HouseholderMatGenWMMA <32>>() {return "wmma_32";}
+template <> std::string get_class_name<HouseholderMatGenWMMAe<32>>() {return "wmmae_32";}
 
 template <unsigned DIM, class HouseholderMatGen>
 void batched_householder(
@@ -142,6 +164,8 @@ void test_batched_kernel(
 int main() {
 	std::printf("batch_size,api,time\n");
 	for (unsigned i = 10; i <= 24; i++) {
+		test_batched_kernel<32, HouseholderMatGenWMMA <32>>(1u << i);
+		test_batched_kernel<32, HouseholderMatGenWMMAe<32>>(1u << i);
 		test_batched_kernel<16, HouseholderMatGenWMMA <16>>(1u << i);
 		test_batched_kernel<16, HouseholderMatGenWMMAe<16>>(1u << i);
 	}
