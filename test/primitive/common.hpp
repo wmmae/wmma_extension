b06d33a0c48bf6e8a4ad92f7887234045ddd2d2b
mutsuki <mutsuki@momo86.net>
2021-08-14 10:27:12 +0900

    Fix `get_string<void>`

--- test/primitive/common.hpp
+++ test/primitive/common.hpp
@@ -11,7 +11,7 @@ template <> std::string get_string<half >() {return "half";}
 template <> std::string get_string<nvcuda::wmma::precision::tf32>() {return "tf32";}
 template <> std::string get_string<nvcuda::wmma::col_major>() {return "col_major";}
 template <> std::string get_string<nvcuda::wmma::row_major>() {return "row_major";}
-template <> std::string get_string<void>() {return "row_major";}
+template <> std::string get_string<void>() {return "void";}
 template <> std::string get_string<nvcuda::wmma::matrix_a>()  {return "matrix_a";}
 template <> std::string get_string<nvcuda::wmma::matrix_b>()  {return "matrix_b";}
 template <> std::string get_string<nvcuda::wmma::accumulator>()  {return "accumulator";}
