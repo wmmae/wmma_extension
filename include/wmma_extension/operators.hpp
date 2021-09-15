80a5a817cfbb6ce937288163fc331d556f8ea732
mutsuki <mutsuki@momo86.net>
2021-09-15 23:31:57 +0900

    Update to use `ops::add` and `ops::sub` in operators.hpp

--- include/wmma_extension/operators.hpp
+++ include/wmma_extension/operators.hpp
@@ -1,26 +1,18 @@
 #ifndef __WMMAE_OPERATORS__
 #define __WMMAE_OPERATORS__
-#include <mma.h>
+#include "detail/operators.hpp"
 
 template <class Use, int M, int N, int K, class Type, class Layout>
 nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> operator+(
 		const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& a,
 		const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& b) {
-	nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& res;
-	for (unsigned i = 0; i < res.num_elements; i++) {
-		res.x[i] = a.x[i] + b.x[i];
-	}
-	return res;
+	return mtk::wmma::detail::add{}(a, b);
 }
 
 template <class Use, int M, int N, int K, class Type, class Layout>
 nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> operator-(
 		const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& a,
 		const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& b) {
-	nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& res;
-	for (unsigned i = 0; i < res.num_elements; i++) {
-		res.x[i] = a.x[i] - b.x[i];
-	}
-	return res;
+	return mtk::wmma::detail::sub{}(a, b);
 }
 #endif
