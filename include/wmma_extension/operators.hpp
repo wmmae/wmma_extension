9e4fc78d89d8bc99ffe6cc8385ad253414aaf1c2
mutsuki <mutsuki@momo86.net>
2021-09-16 20:08:14 +0900

    Add including detail/common.hpp

--- include/wmma_extension/operators.hpp
+++ include/wmma_extension/operators.hpp
@@ -1,6 +1,7 @@
 #ifndef __WMMAE_OPERATORS__
 #define __WMMAE_OPERATORS__
 #include "detail/operators.hpp"
+#include "detail/common.hpp"
 
 template <class Use, int M, int N, int K, class Type, class Layout>
 nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> operator+(
