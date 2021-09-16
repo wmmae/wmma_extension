1b21ddf535c9ec1653375cf046f5ef99ced49187
mutsuki <mutsuki@momo86.net>
2021-09-16 20:07:14 +0900

    Update `detail::add` and `detail::sub`

--- include/wmma_extension/detail/operators.hpp
+++ include/wmma_extension/detail/operators.hpp
@@ -5,12 +5,14 @@
 namespace mtk {
 namespace wmma {
 namespace ops {
+
+// Add
 template <class Use, int M, int N, int K, class Type, class Layout>
 struct add {
 	nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> operator()(
 			const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& a,
 			const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& b) {
-		nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& res;
+		nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> res;
 		for (unsigned i = 0; i < res.num_elements; i++) {
 			res.x[i] = a.x[i] + b.x[i];
 		}
@@ -20,10 +22,10 @@ struct add {
 
 template <class Use, int M, int N, int K, class Layout>
 struct add<Use, M, N, K, half, Layout> {
-	nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> operator()(
-			const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& a,
-			const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& b) {
-		nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& res;
+	nvcuda::wmma::fragment<Use, M, N, K, half, Layout> operator()(
+			const nvcuda::wmma::fragment<Use, M, N, K, half, Layout>& a,
+			const nvcuda::wmma::fragment<Use, M, N, K, half, Layout>& b) {
+		nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> res;
 		for (unsigned i = 0; i < res.num_elements / 2; i++) {
 			reinterpret_cast<half2>(res.x)[i] = __hadd2(reinterpret_cast<half2>(a.x)[i], reinterpret_cast<half2>(b.x)[i]);
 		}
@@ -31,12 +33,13 @@ struct add<Use, M, N, K, half, Layout> {
 	}
 };
 
+// Sub
 template <class Use, int M, int N, int K, class Type, class Layout>
 struct sub {
 	nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> operator()(
 			const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& a,
 			const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& b) {
-		nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& res;
+		nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> res;
 		for (unsigned i = 0; i < res.num_elements; i++) {
 			res.x[i] = a.x[i] + b.x[i];
 		}
@@ -46,10 +49,10 @@ struct sub {
 
 template <class Use, int M, int N, int K, class Layout>
 struct sub<Use, M, N, K, half, Layout> {
-	nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> operator()(
-			const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& a,
-			const nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& b) {
-		nvcuda::wmma::fragment<Use, M, N, K, Type, Layout>& res;
+	nvcuda::wmma::fragment<Use, M, N, K, half, Layout> operator()(
+			const nvcuda::wmma::fragment<Use, M, N, K, half, Layout>& a,
+			const nvcuda::wmma::fragment<Use, M, N, K, half, Layout>& b) {
+		nvcuda::wmma::fragment<Use, M, N, K, Type, Layout> res;
 		for (unsigned i = 0; i < res.num_elements / 2; i++) {
 			reinterpret_cast<half2>(res.x)[i] = __hsub2(reinterpret_cast<half2>(a.x)[i], reinterpret_cast<half2>(b.x)[i]);
 		}
