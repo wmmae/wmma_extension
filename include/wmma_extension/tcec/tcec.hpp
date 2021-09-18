307617aabd984591ee70acaa5af8da6fc588023d
mutsuki <mutsuki@momo86.net>
2021-09-18 21:28:23 +0900

    Fix dx scaling in `print_fragment`

--- include/wmma_extension/tcec/tcec.hpp
+++ include/wmma_extension/tcec/tcec.hpp
@@ -4,22 +4,11 @@
 #include "detail/common.hpp"
 #include "detail/policy.hpp"
 #include "detail/functions.hpp"
+#include "detail/scale.hpp"
 
 namespace mtk {
 namespace wmma {
 namespace tcec {
-
-namespace detail {
-template <class T>
-__device__ inline float correction_scale_0(const float v) {return v;}
-template <>
-__device__ inline float correction_scale_0<half>(const float v) {return v * 2048;}
-
-template <class T>
-__device__ inline float correction_scale_1(const float v) {return v;}
-template <>
-__device__ inline float correction_scale_1<half>(const float v) {return v / 2048;}
-} // namespace detail
 
 template <class Use, int m, int n, int k, class T, class Layout = void,
 		 class Policy_ = typename mtk::wmma::tcec::detail::default_policy<T>::type>
