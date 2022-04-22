19f5dba85071f9cdce4e492d9aecd806a025f6e0
mutsuki <mutsuki@momo86.net>
2022-04-22 19:17:23 +0900

    Add C shared memory bank conflict test

--- research/bank-conflict/main.cu
+++ research/bank-conflict/main.cu
@@ -11,11 +11,14 @@ namespace {
 template <class T>
 std::string get_name_str();
 template <> std::string get_name_str<half>() {return "half";}
+template <> std::string get_name_str<float>() {return "float";}
 template <> std::string get_name_str<nvcuda::wmma::precision::tf32>() {return "tf32";}
 template <> std::string get_name_str<nvcuda::wmma::matrix_a>() {return "matrix_a";}
 template <> std::string get_name_str<nvcuda::wmma::matrix_b>() {return "matrix_b";}
+template <> std::string get_name_str<nvcuda::wmma::accumulator>() {return "accumulator";}
 template <> std::string get_name_str<nvcuda::wmma::col_major>() {return "col_major";}
 template <> std::string get_name_str<nvcuda::wmma::row_major>() {return "row_major";}
+template <> std::string get_name_str<void>() {return "void";}
 }
 
 template <class Use, int m, int n, int k, class T, class Layout>
@@ -28,24 +31,28 @@ __global__ void kernel(
 	FRAG_T frag;
 
 	unsigned frag_i = 0;
-	mtk::wmma::mma::foreach_ij<FRAG_T>(
-			[&](const unsigned* frag_index_list, const unsigned fragment_index_count, const unsigned i, const unsigned j) {
-			  const unsigned mem_index = (layout == nvcuda::wmma::mem_col_major) ? (i + j * ldm) : (j + i * ldm);
-				const unsigned bank = mem_index % bank_size;
+	const auto func = [&](const unsigned* frag_index_list, const unsigned fragment_index_count, const unsigned i, const unsigned j) {
+		const unsigned mem_index = (layout == nvcuda::wmma::mem_col_major) ? (i + j * ldm) : (j + i * ldm);
+		const unsigned bank = mem_index % bank_size;
 
-				atomicAdd(&bank_array_ptr[(frag_i++) * bank_size + bank], 1);
+		atomicAdd(&bank_array_ptr[(frag_i++) * bank_size + bank], 1);
 
-				for (unsigned t = 0; t < bank_size; t++) {
-					if (t == threadIdx.x) {
-						printf("%3u(%03x) ", bank, mem_index);
-					}
-					__syncthreads();
-				}
-				if (threadIdx.x == 0) {
-					printf("\n");
-				}
-				__syncthreads();
-			});
+		for (unsigned t = 0; t < bank_size; t++) {
+			if (t == threadIdx.x) {
+				printf("%3u(%03x) ", bank, mem_index);
+			}
+			__syncthreads();
+		}
+		if (threadIdx.x == 0) {
+			printf("\n");
+		}
+		__syncthreads();
+	};
+	if constexpr (std::is_same<Use, nvcuda::wmma::accumulator>::value) {
+		mtk::wmma::mma::foreach_ij<FRAG_T>(layout, func);
+	} else {
+		mtk::wmma::mma::foreach_ij<FRAG_T>(func);
+	}
 }
 
 template <class Use, int m, int n, int k, class T, class Layout>
@@ -83,9 +90,13 @@ int main() {
 	print_bank_conflict<nvcuda::wmma::matrix_a, 16, 8, 16, half, nvcuda::wmma::row_major>(nvcuda::wmma::mem_row_major, ldm);
 	print_bank_conflict<nvcuda::wmma::matrix_b, 16, 8, 16, half, nvcuda::wmma::col_major>(nvcuda::wmma::mem_col_major, ldm);
 	print_bank_conflict<nvcuda::wmma::matrix_b, 16, 8, 16, half, nvcuda::wmma::col_major>(nvcuda::wmma::mem_row_major, ldm);
+	print_bank_conflict<nvcuda::wmma::accumulator, 16, 8, 16, float, void>(nvcuda::wmma::mem_col_major, ldm);
+	print_bank_conflict<nvcuda::wmma::accumulator, 16, 8, 16, float, void>(nvcuda::wmma::mem_row_major, ldm);
 
 	print_bank_conflict<nvcuda::wmma::matrix_a, 16, 8, 8, nvcuda::wmma::precision::tf32, nvcuda::wmma::row_major>(nvcuda::wmma::mem_col_major, ldm);
 	print_bank_conflict<nvcuda::wmma::matrix_a, 16, 8, 8, nvcuda::wmma::precision::tf32, nvcuda::wmma::row_major>(nvcuda::wmma::mem_row_major, ldm);
 	print_bank_conflict<nvcuda::wmma::matrix_b, 16, 8, 8, nvcuda::wmma::precision::tf32, nvcuda::wmma::col_major>(nvcuda::wmma::mem_col_major, ldm);
 	print_bank_conflict<nvcuda::wmma::matrix_b, 16, 8, 8, nvcuda::wmma::precision::tf32, nvcuda::wmma::col_major>(nvcuda::wmma::mem_row_major, ldm);
+	print_bank_conflict<nvcuda::wmma::accumulator, 16, 8, 8, float, void>(nvcuda::wmma::mem_col_major, ldm);
+	print_bank_conflict<nvcuda::wmma::accumulator, 16, 8, 8, float, void>(nvcuda::wmma::mem_row_major, ldm);
 }
