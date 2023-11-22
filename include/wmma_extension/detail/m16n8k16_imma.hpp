51161428219b3dbb79424858af4bfc9fbe2bfa32
mutsuki <mutsuki@momo86.net>
2023-11-22 22:04:14 +0900

    Add foreach and foreach_j for imma

--- include/wmma_extension/detail/m16n8k16_imma.hpp
+++ include/wmma_extension/detail/m16n8k16_imma.hpp
@@ -13,6 +13,91 @@ template <> class fragment<nvcuda::wmma::matrix_b   , 16, 8, 16, std::uint8_t, n
 template <> class fragment<nvcuda::wmma::accumulator, 16, 8, 16, std::int32_t> : public __frag_base<std::int32_t, 4>{};
 
 // foreach
+template <class Func>
+__device__ inline void foreach(mtk::wmma::mma::fragment<nvcuda::wmma::matrix_a, 16, 8, 16, std::int8_t, nvcuda::wmma::row_major>& frag, Func func) {
+	const unsigned col = (mtk::wmma::detail::common::get_lane_id() % 4) * 4;
+	const unsigned row = mtk::wmma::detail::common::get_lane_id() / 16;
+	for (unsigned i = 0; i < 4; i++) {
+    {const unsigned frag_index_list[1] = {i + 0};func(frag_index_list, 1, col + i + (row + 0) * 16);}
+    {const unsigned frag_index_list[1] = {i + 4};func(frag_index_list, 1, col + i + (row + 8) * 16);}
+  }
+}
+
+template <class Func>
+__device__ inline void foreach(mtk::wmma::mma::fragment<nvcuda::wmma::matrix_b, 16, 8, 16, std::int8_t, nvcuda::wmma::col_major>& frag, Func func) {
+	const unsigned col = mtk::wmma::detail::common::get_lane_id() / 4;
+	const unsigned row = (mtk::wmma::detail::common::get_lane_id() % 4) * 4;
+	for (unsigned i = 0; i < 4; i++) {
+    {const unsigned frag_index_list[1] = {i};func(frag_index_list, 1, row + i + col * 16);}
+  }
+}
+
+template <class Func>
+__device__ inline void foreach(mtk::wmma::mma::fragment<nvcuda::wmma::accumulator, 16, 8, 16, std::int32_t>& frag, const nvcuda::wmma::layout_t layout, Func func) {
+	const unsigned col = (mtk::wmma::detail::common::get_lane_id() % 4) * 2;
+	const unsigned row_offset = mtk::wmma::detail::common::get_lane_id() / 4;
+
+	for (unsigned i = 0; i < 2; i++) {
+		const auto row = row_offset + i * 8;
+		if (layout == nvcuda::wmma::mem_col_major) {
+			{const unsigned frag_index_list[1] = {(i * 2 + 0)};func(frag_index_list, 1, row + (col + 0) * 16);}
+			{const unsigned frag_index_list[1] = {(i * 2 + 1)};func(frag_index_list, 1, row + (col + 1) * 16);}
+		} else {
+			{const unsigned frag_index_list[1] = {(i * 2 + 0)};func(frag_index_list, 1, row * 8 + (col + 0));}
+			{const unsigned frag_index_list[1] = {(i * 2 + 1)};func(frag_index_list, 1, row * 8 + (col + 1));}
+		}
+	}
+}
+
+// foreach_ij
+template <class Func>
+__device__ inline void foreach_ij(mtk::wmma::mma::fragment<nvcuda::wmma::matrix_a, 16, 8, 16, std::int8_t, nvcuda::wmma::row_major>& frag, Func func) {
+	const unsigned col = (mtk::wmma::detail::common::get_lane_id() % 4) * 4;
+	const unsigned row = mtk::wmma::detail::common::get_lane_id() / 16;
+
+	for (unsigned i = 0; i < 4; i++) {
+    {const unsigned frag_index_list[1] = {i + 0};func(frag_index_list, 1, (row + 0), col + i);}
+    {const unsigned frag_index_list[1] = {i + 4};func(frag_index_list, 1, (row + 8), col + i);}
+  }
+}
+
+template <class Func>
+__device__ inline void foreach_ij(mtk::wmma::mma::fragment<nvcuda::wmma::matrix_b, 16, 8, 16, std::int8_t, nvcuda::wmma::col_major>& frag, Func func) {
+	const unsigned col = mtk::wmma::detail::common::get_lane_id() / 4;
+	const unsigned row = (mtk::wmma::detail::common::get_lane_id() % 4) * 4;
+
+	for (unsigned i = 0; i < 4; i++) {
+    {const unsigned frag_index_list[1] = {i};func(frag_index_list, 1, row + i, col);}
+  }
+}
+
+template <class Func>
+__device__ inline void foreach_ij(mtk::wmma::mma::fragment<nvcuda::wmma::accumulator, 16, 8, 16, std::int32_t>& frag, const nvcuda::wmma::layout_t layout, Func func) {
+	const unsigned col = (mtk::wmma::detail::common::get_lane_id() % 4) * 2;
+	const unsigned row_offset = mtk::wmma::detail::common::get_lane_id() / 4;
+
+	for (unsigned i = 0; i < 2; i++) {
+		const auto row = row_offset + i * 8;
+		if (layout == nvcuda::wmma::mem_col_major) {
+			{const unsigned frag_index_list[1] = {(i * 2 + 0)};func(frag_index_list, 1, row, col + 0);}
+			{const unsigned frag_index_list[1] = {(i * 2 + 1)};func(frag_index_list, 1, row, col + 1);}
+		} else {
+			{const unsigned frag_index_list[1] = {(i * 2 + 0)};func(frag_index_list, 1, row, col + 0);}
+			{const unsigned frag_index_list[1] = {(i * 2 + 1)};func(frag_index_list, 1, row, col + 1);}
+		}
+	}
+}
+
+// alias
+template <class Func>
+__device__ inline void foreach(mtk::wmma::mma::fragment<nvcuda::wmma::matrix_a, 16, 8, 16, std::uint8_t, nvcuda::wmma::row_major>& frag, Func func) {
+  foreach(*reinterpret_cast<mtk::wmma::mma::fragment<nvcuda::wmma::matrix_a, 16, 8, 16, std::int8_t, nvcuda::wmma::row_major>*>(&frag), func);
+}
+
+template <class Func>
+__device__ inline void foreach(mtk::wmma::mma::fragment<nvcuda::wmma::matrix_b, 16, 8, 16, std::uint8_t, nvcuda::wmma::col_major>& frag, Func func) {
+  foreach(*reinterpret_cast<mtk::wmma::mma::fragment<nvcuda::wmma::matrix_b, 16, 8, 16, std::int8_t, nvcuda::wmma::col_major>*>(&frag), func);
+}
 
 __device__ inline void mma_sync(
 		fragment<nvcuda::wmma::accumulator, 16, 8, 16, std::int32_t>& d,
@@ -26,11 +111,11 @@ __device__ inline void mma_sync(
       {%6, %7},
       {%8, %9, %10, %11};
 })"
-			: "=f"(d.x[0]), "=f"(d.x[1]), "=f"(d.x[2]), "=f"(d.x[3])
+			: "=r"(d.x[0]), "=r"(d.x[1]), "=r"(d.x[2]), "=r"(d.x[3])
 			: "r"(*reinterpret_cast<const unsigned*>(a.x)),
 			"r"(*reinterpret_cast<const unsigned*>(a.x + 2)),
 			"r"(*reinterpret_cast<const unsigned*>(b.x)),
-			"f"(c.x[0]), "f"(c.x[1]), "f"(c.x[2]), "f"(c.x[3]));
+			"r"(c.x[0]), "r"(c.x[1]), "r"(c.x[2]), "r"(c.x[3]));
 }
 
 __device__ inline void mma_sync(
@@ -45,12 +130,13 @@ __device__ inline void mma_sync(
       {%6, %7},
       {%8, %9, %10, %11};
 })"
-			: "=f"(d.x[0]), "=f"(d.x[1]), "=f"(d.x[2]), "=f"(d.x[3])
+			: "=r"(d.x[0]), "=r"(d.x[1]), "=r"(d.x[2]), "=r"(d.x[3])
 			: "r"(*reinterpret_cast<const unsigned*>(a.x)),
 			"r"(*reinterpret_cast<const unsigned*>(a.x + 2)),
 			"r"(*reinterpret_cast<const unsigned*>(b.x)),
-			"f"(c.x[0]), "f"(c.x[1]), "f"(c.x[2]), "f"(c.x[3]));
+			"r"(c.x[0]), "r"(c.x[1]), "r"(c.x[2]), "r"(c.x[3]));
 }
 } // namespace mma
 } // namespace wmma
 } // namespace mtk
+#endif
